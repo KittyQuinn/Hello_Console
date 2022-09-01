956a95af410694d24b6f74012990f803619c4993
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-09-01 16:28:42 +0200

    Added Example 005 Condition

---
+++ Examples005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Write username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Yeah, this is Masha!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
