f65a0dfafc955b1f24f21c4c8b4f66d94fe493fe
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-09-06 18:05:59 +0200

    Added array

---
+++ Example009_Array/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {60,20,9,14,73,20,77};
+int n = array.Length;
+int find = 20;
+int index = 0;
+
+while(index<n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
